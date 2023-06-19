ba2f2829fe619e1c66ac17d3078620fa8eadfa8b
Pedro Victor <pedro@fflank.com>
2023-06-18 22:58:15 -0300

    UPDATE - Header

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -1,10 +1,16 @@
 "use client";
+
+import { User } from "lucide-react";
+
 export function Header() {
   return (
-    <header className="flex items-center justify-between p-4 bg-slate-200 text-black">
-      <div className="flex flex-col">
+    <header className="flex items-center justify-between p-4 bg-slate-200 text-black font-mono w-full">
+      <div className="flex gap-2">
+        <User className="h-5 w-5 text-gray-700"/>
         <a href="">Entrar</a>
-        <a href="">Não tem uma conta? Clique aqui</a>
+      </div>
+      <div>
+        <h1 className="text-3xl">Reorganize</h1>
       </div>
       <div>
          <button>Novo projeto</button>
